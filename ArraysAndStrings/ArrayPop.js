f47514e4ffd287e82b1cf719877caad54f978fac
nadigerdivya <nadigerdivya@gmail.com>
2021-12-09 08:31:42 -0800

    Adding practic problems

---
+++ ArraysAndStrings/ArrayPop.js
@@ -0,0 +1,24 @@
+//ArrayPop.js
+////Implemeting Array Pop() method. - Pop() method removes (pops) the last element of an array and 
+////returns the returns the removed element.
+
+function Pop(array){
+    let length = findLength(array);
+    let lastItem = array[length - 1];
+    delete array[length - 1];
+    return lastItem;
+}
+
+//Returns the length of the array
+function findLength(array){
+    let count = 0;
+
+    while(array[count] != null){
+        count++;
+    }
+
+    return count;
+}
+
+const removedElement = Pop(['a', 'b', 'c', 'd']);
+console.log(removedElement);
